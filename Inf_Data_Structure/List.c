9bdbd66b621149b636d6048a2af2bf6ef9a9920e
github-username <jhkim20000103@gmail.com>
2022-01-04 19:17:29 +0900

    please

---
+++ Inf_Data_Structure/List.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+struct node
+{
+    char *data;
+    struct node *next; 
+};
+
+typedef struct node Node;
+Node *head = NULL; // 연결리스트의 첫 번째 노드의 주소를 저장할 포인터이다.
+int main()
+{
+    
+    return 0;
+}
