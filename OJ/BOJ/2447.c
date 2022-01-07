29d2c1914a2ad89a5511d0bb44681049032a2ed3
github-username <jhkim20000103@gmail.com>
2022-01-07 15:10:49 +0900

    so sick

--- OJ/BOJ/2447.c
+++ OJ/BOJ/2447.c
@@ -1,8 +1,24 @@
+// BOJ_2447 별 찍기 - 10
 #include <stdio.h>
 #include <math.h>
+void star(int i, int j, int N);
 int main(void)
 {
+    int N, i, j;
+    int map[2188][2188];
+    scanf("%d", &N);
+    for (i = 0; i < 2188; i++)
+    {
+        for (j = 0; j < 2188; j++)
+        {
+            map[i][j] = ' ';
+        }
+    }
 
-    printf("%.f\n",pow(3,8));
+    star(0,0,N);
+
     return 0;
+}
+void star(int i, int j, int N)
+{
 }
