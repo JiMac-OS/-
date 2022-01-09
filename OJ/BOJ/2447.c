4f6ffef4158f18bb8a4d728625cd6e3300973bb9
github-username <jhkim20000103@gmail.com>
2022-01-10 01:15:15 +0900

    restart

--- OJ/BOJ/2447.c
+++ OJ/BOJ/2447.c
@@ -1,24 +1,33 @@
 // BOJ_2447 별 찍기 - 10
 #include <stdio.h>
-#include <math.h>
 void star(int i, int j, int N);
 int main(void)
 {
-    int N, i, j;
-    int map[2188][2188];
-    scanf("%d", &N);
-    for (i = 0; i < 2188; i++)
+    int n, i, j;
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < 2188; j++)
+        for (j = 0; j < n; j++)
         {
-            map[i][j] = ' ';
+            star(i, j, n);
         }
+        printf("\n");
     }
-
-    star(0,0,N);
-
-    return 0;
 }
 void star(int i, int j, int N)
 {
+    if ((i / N) % 3 == 1 && (j / N) % 3 == 1)
+    {
+        printf(" ");
+    }
+    else
+    {
+        if (N / 3 == 0)
+            printf("*");
+        else
+        {
+            star(i, j, N / 3);
+        }
+    }
 }
