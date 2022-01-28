4080473bad41db1db4db9af3d17d4bf08715ca4f
github-username <jhkim20000103@gmail.com>
2022-01-28 23:42:44 +0900

    doble linked list practice

--- OJ/exOJ/연결리스트(1)/1.c
+++ OJ/exOJ/연결리스트(1)/1.c
@@ -10,19 +10,65 @@ typedef struct node
 void add(Node *list, int r, char e);
 void print(Node *head);
 void delete (Node *list, int r);
+Node *get(Node *list, int r);
 int main(void)
 {
     Node *head = (Node *)malloc(sizeof(Node));
     Node *tail = (Node *)malloc(sizeof(Node));
     head->next = tail;
     tail->prev = head;
-    add(head, 1, 'S');
-    add(head, 2, 't');
-    add(head, 3, 'a');
-    add(head, 2, 'r');
-    print(head);
+
+    int n, r;
+    char e, c;
+    scanf("%d", &n);
+    getchar();
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%c", &c);
+        getchar();
+        switch (c)
+        {
+        case 'A':
+            scanf("%d %c", &r, &e);
+            getchar();
+            add(head, r, e);
+            break;
+        case 'D':
+            scanf("%d", &r);
+            getchar();
+            delete (head, r);
+            break;
+        case 'G':
+            scanf("%d", &r);
+            getchar();
+            Node *p = get(head, r);
+            if (p == tail)
+                printf("invalid position\n");
+            else
+                printf("%c\n", p->elem);
+            break;
+        case 'P':
+            print(head);
+        default:
+            break;
+        }
+    }
 
     return 0;
+}
+Node *get(Node *list, int r)
+{
+    Node *p = list;
+    for (int i = 0; i < r; i++)
+    {
+        if (p->next == NULL)
+        {
+            printf("invalid position\n");
+            return NULL;
+        }
+        p = p->next;
+    }
+    return p;
 }
 void print(Node *head)
 {
@@ -36,16 +82,7 @@ void print(Node *head)
 }
 void add(Node *list, int r, char e)
 {
-    Node *p = list;
-    for (int i = 0; i < r; i++)
-    {
-        if (p->next == NULL)
-        {
-            printf("invaild position\n");
-            return;
-        }
-        p = p->next;
-    }
+    Node *p = get(list, r);
 
     Node *new_node = (Node *)malloc(sizeof(Node));
     new_node->elem = e;
@@ -58,4 +95,28 @@ void add(Node *list, int r, char e)
 }
 void delete (Node *list, int r)
 {
+    Node *p = get(list, r);
+    p->prev->next = p->next;
+    p->next->prev = p->prev;
+    if (p->next == NULL || r == 0)
+    {
+        printf("invalid position\n");
+        return;
+    }
+    free(p);
 }
+
+/* 
+9 
+A 1 D 
+A 2 a 
+A 3 y 
+D 1 
+P 
+G 3 
+A 1 S 
+P 
+G 3 
+ */
+
+//https://engeon.tistory.com/75 꼭 참고하자
