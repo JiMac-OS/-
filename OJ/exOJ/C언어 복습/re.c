6316e7097620aba00a717f03e919627126a95718
github-username <jhkim20000103@gmail.com>
2022-03-25 12:28:13 +0900

    3/25

--- OJ/exOJ/C언어 복습/re.c
+++ OJ/exOJ/C언어 복습/re.c
@@ -1,54 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-void change(int *arr, int from, int to);
 int main(void)
 {
-    int *arr, *p;
-    int N, cnt, from, to;
-    arr = (int *)malloc(sizeof(int) * 101);
+    int arr[100][100];
+    int N, v = 1;
     scanf("%d", &N);
-    for (p = arr; p < arr + N; p++)
-        scanf("%d", p);
-
-    scanf("%d", &cnt);
-    for (int i = 0; i < cnt; i++)
+    for (int i = 0; i < N; i++)
     {
-        scanf("%d %d", &from, &to);
-        change(arr, from, to);
-    }
-
-    for (p = arr; p < arr + N; p++)
-        printf(" %d", *p);
-    printf("\n");
-    free(arr);
-    return 0;
-}
-void change(int *arr, int from, int to)
-{
-    int i = from, j = to, tmp;
-    int gap = to - from;
-    if (gap % 2 == 0)
-    {
-        int count = gap / 2;
-        while (count--)
+        if (i % 2 == 0)
         {
-            tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-            i++;
-            j--;
+            for (int j = 0; j < N; j++)
+            {
+                arr[i][j] = v;
+                v++;
+            }
+        }
+        else
+        {
+            for (int j = N - 1; j >= 0; j--)
+            {
+                arr[i][j] = v;
+                v++;
+            }
         }
     }
-    else
+    for (int i = 0; i < N; i++)
     {
-        int count = gap / 2 + 1;
-        while (count--)
-        {
-            tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-            i++;
-            j--;
-        }
+        for (int j = 0; j < N; j++)
+            printf(" %d", arr[i][j]);
+        printf("\n");
     }
 }
